48d1896361e204b3a046390696fa5993616a5f1b
Jhow17 <silvajonathan234@gmail.com>
2024-10-13 20:26:58 -0300

    feat: adicionar pastas 'pages', 'styles' e 'scripts' ao projeto

---
+++ src/scripts/script.js
@@ -0,0 +1,52 @@
+function Enviar() {
+    var nome = document.querySelector("#nome").value.trim();
+    var email = document.querySelector("#email").value.trim();
+    var telefone = document.querySelector("#phone").value.trim();
+    var assunto = document.querySelector("#assunto").value;
+    var mensagem = document.querySelector("#mensagem").value.trim();
+    // Exibindo os dados em um alert
+    if (nome.length < 2)
+    {
+        alert("O nome deve conter pelo menos duas letras.")
+    }
+    else if (email === ""){
+        alert("O email não pode ficar vazio.");
+
+    }
+    else if (telefone === ""){
+        alert("O telefone não pode ficar vazio.");
+        
+    }
+    else if (assunto === ""){
+        alert("Você deve selecionar um assunto.");
+        
+    }
+    else if (mensagem === ""){
+        alert("A mensagem não pode ficar em branco.");
+        
+    }
+    else{
+        switch(assunto){
+            case 'adocao':
+                assunto = 'Adoção';
+                break;
+            case 'ajuda':
+                assunto = 'Ajuda';
+                break;
+            case 'prox':
+                assunto = 'Próximos Eventos';
+                break;
+            case 'suges':
+                assunto = 'Sugestões';
+                break;
+            case 'outros':
+                assunto = 'Outros';
+                break;
+            default:
+                assunto = 'INVALIDO';
+                break;
+        }
+            window.alert(
+        `Informações preenchidas:\n Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nAssunto: ${assunto}\nMensagem: ${mensagem}\n\nEnviado com sucesso!`);
+    }
+}
